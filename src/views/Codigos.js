e991a11acc458f78c9e90549b639cd35745ac248
campbelt15 <ottoniel.campos@nuestrodiario.com.gt>
2024-07-09 08:08:09 -0600

    Codigos: se aplico clean code a la vista de Codigos.js

--- src/views/Codigos.js
+++ src/views/Codigos.js
@@ -7,140 +7,129 @@ import { Column } from 'primereact/column'
 import { InputText } from 'primereact/inputtext'
 
 const Codigos = () => {
-    const reactivateApi = process.env.REACT_APP_REACTIVATE_API
-    const codesListApi = process.env.REACT_APP_LISTAR_CODIGOS_API
+  const REACTIVATEAPI = process.env.REACT_APP_REACTIVATE_API
+  const CODESLISTAPI = process.env.REACT_APP_LISTAR_CODIGOS_API
 
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, setData] = useState([])
-    const [globalFilter, setGlobalFilter] = useState(null)
-    const userEmail = localStorage.getItem('userEmail')
-    const userIP = localStorage.getItem('userIP')
+  const [isLoading, setIsLoading] = useState(true)
+  const [codesData, setCodesData] = useState([])
+  const [globalFilter, setGlobalFilter] = useState(null)
+  const userEmail = localStorage.getItem('userEmail')
+  const userIP = localStorage.getItem('userIP')
 
-    const loadData = () => {
-      fetch(codesListApi)
-        .then(response => response.json())
-        .then(data => {
-          const responseBody = JSON.parse(data.body)
-          setData(responseBody.Items)
-        })
-        .catch(error => {
-          console.error('Error al cargar los datos:', error)
-        })
-        .finally(() => {
-          setIsLoading(false)
-        })
+  const fetchCodesData = async () => {
+    try {
+      const response = await fetch(CODESLISTAPI)
+      const data = await response.json()
+      const responseBody = JSON.parse(data.body)
+      setCodesData(responseBody.Items)
+    } catch (error) {
+      console.error('Error al cargar los datos:', error)
+    } finally {
+      setIsLoading(false)
     }
-    
-    
-    useEffect(() => {
-      loadData()
-    }, [])
+  }
 
-    const handleActivateClick = async (itemData) => {
+  useEffect(() => {
+    fetchCodesData()
+  }, [])
 
-      const data = {
-        ...itemData,
-        correo : userEmail,
-        ip: userIP
-      }
+  const swalSuccess = (title, text) => {
+    swal({
+      title,
+      text,
+      icon: "success",
+      button: "OK",
+      timer: 3000
+    })
+  }
 
-      try {
-        const response = await fetch(
-          reactivateApi,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-          }
-        )
+  const swalError = (title, text) => {
+    swal({
+      title,
+      text,
+      icon: "error",
+      button: "OK",
+      timer: 3000
+    })
+  }
 
-        if (response.ok) {
-          swal({
-            title: "Reactivado",
-            text: "Reactivado correctamente.",
-            icon: "success",
-            button: "OK",
-            timer: "3000"
-          })
-          loadData()
-        } else {
-          swal({
-            title: "Error al reactivar",
-            text: response.statusText,
-            icon: "warning",
-            button: "OK",
-            timer: "3000"
-          })
-
-        }
-      } catch (error) {
-        // Manejar errores aquí
-        swal({
-          title: "Error al reactivar",
-          text: error,
-          icon: "warning",
-          button: "OK",
-          timer: "3000"
-        })
-        console.error("Error al actualizar los datos", error)
-      }
+  const handleCodeActivation = async (itemData) => {
+    const data = {
+      ...itemData,
+      correo: userEmail,
+      ip: userIP
     }
 
-    const header = (
-      <>
-      <div className="fs-2 mb-3">
-        Lista de Códigos
-        <span className="p-input-icon-left ms-2">
-          <i className="pi pi-search" />
-          <InputText 
+    try {
+      const response = await fetch(REACTIVATEAPI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+
+      if (response.ok) {
+        swalSuccess("Reactivado", "Reactivado correctamente.")
+        fetchCodesData()
+      } else {
+        swalError("Error al reactivar", response.statusText)
+      }
+    } catch (error) {
+      swalError("Error al reactivar", error.message)
+      console.error("Error al actualizar los datos", error)
+    }
+  }
+
+  const renderHeader = () => (
+    <div className="fs-2 mb-3">
+      Lista de Códigos
+      <span className="p-input-icon-left ms-2">
+        <i className="pi pi-search" />
+        <InputText 
           type="search" 
-          onInput={(e) => setGlobalFilter(e.target.value)} placeholder="Buscar..." />
-        </span>
+          onInput={(e) => setGlobalFilter(e.target.value)} 
+          placeholder="Buscar..." 
+        />
+      </span>
+    </div>
+  )
+
+  const renderFooter = () => (
+    <div className="table-footer">
+      Total de códigos: {codesData.length}
+    </div>
+  )
+
+  const renderActionBody = (rowData) => {
+    const currentDate = new Date().toISOString().split('T')[0]
+    const isExpired = rowData.date_exp && (rowData.date_exp > currentDate)
+
+    return (
+      <div>
+        <button 
+          type="button" 
+          className="btn btn-primary"
+          onClick={() => handleCodeActivation(rowData)}
+          disabled={isExpired}
+        >
+          Activar
+        </button>
       </div>
-      </>
     )
-    
-    
-    const footer = (
-      <div className="table-footer">
-        Total de códigos: {data.length}
-      </div>
-    )
-
-    const actionBody = (rowData) => {
-      // Obtener la fecha actual en el formato adecuado
-      const currentDate = new Date().toISOString().split('T')[0]
-
-      // Comprobar si date_exp está vacío o es mayor a la fecha actual
-      const isExpired = rowData.date_exp && (rowData.date_exp > currentDate)
-
-      return (
-        <div>
-          <button 
-            type="button" 
-            className="btn btn-primary"
-            onClick={() => handleActivateClick(rowData)}
-            disabled={isExpired} // Desactivar el botón si la fecha ha expirado
-          >
-            Activar
-          </button>
-        </div>
-      )
-    }
+  }
 
   return (
     <>
-    {isLoading ? (
-      <p>Cargando datos...</p> 
-    ) : (
-      <DataTable 
-          value={data}
+      {isLoading ? (
+        <p>Cargando datos...</p>
+      ) : (
+        <DataTable 
+          value={codesData}
           dataKey="id"
           paginator
           showGridlines
-          globalFilterFields={['nombre', 'producto', 'codigo', 'telefono', 'responsable']}//agregar los tipos de busqueda
+          globalFilterFields={['nombre', 'producto', 'codigo', 'telefono', 'responsable']}
           globalFilter={globalFilter}
           rows={10}
           sortField="date"
@@ -149,22 +138,19 @@ const Codigos = () => {
           rowsPerPageOptions={[5, 10, 25, 50]}
           tableStyle={{ minWidth: '30rem', lineHeight: '1.5' }}
           className="custom_table"
-          header={header}
-          footer={footer}
+          header={renderHeader()}
+          footer={renderFooter()}
         >
-            
-            {/* <Column field="responsable" className="d-none d-xxl-table-cell me-1" headerClassName="d-none d-xxl-table-cell" sortable header="Responsable"></Column> */}
-            <Column field="codigo" bodyClassName="" header="Código" ></Column>
-            <Column field="date" bodyClassName="hidden" headerClassName='hidden'></Column>
-            <Column field="fecha_actual" className="d-none d-xxl-table-cell" headerClassName="d-none d-xxl-table-cell" sortable header="Fecha Creación"></Column>
-            <Column field="hora_actual" bodyClassName="" sortable header="Hora Creación"></Column>
-            <Column field="activate_exp" className="d-none d-xxl-table-cell" headerClassName="d-none d-xxl-table-cell" sortable header="Expiración Código"></Column>
-            <Column field="nombre" bodyClassName="" sortable header="Cliente"></Column>
-            <Column field="producto" bodyClassName="" sortable header="Producto"></Column>
-            <Column field="telefono" bodyClassName="" header="Teléfono"></Column>
-            <Column field="date_exp" bodyClassName="" header="Expiración Plan"></Column>
-            <Column field="actions" header="Acción" body={actionBody}></Column>
-            
+          <Column field="codigo" header="Código" />
+          <Column field="date" className="hidden" headerClassName='hidden' />
+          <Column field="fecha_actual" bodyClassName="d-none d-xxl-table-cell" headerClassName="d-none d-xxl-table-cell" sortable header="Fecha Creación" />
+          <Column field="hora_actual" sortable header="Hora Creación" />
+          <Column field="activate_exp" bodyClassName="d-none d-xxl-table-cell" headerClassName="d-none d-xxl-table-cell" sortable header="Expiración Código" />
+          <Column field="nombre" sortable header="Cliente" />
+          <Column field="producto" sortable header="Producto" />
+          <Column field="telefono" header="Teléfono" />
+          <Column field="date_exp" header="Expiración Plan" />
+          <Column field="actions" header="Acción" body={renderActionBody} />
         </DataTable>
       )}
     </>
